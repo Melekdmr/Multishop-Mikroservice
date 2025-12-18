7d3fb158caa11236f046d50395e21f40e5758b97
MELEK DEMİR <melekdmrr01@gmail.com>
2025-12-18 22:33:29 +0300

    feat(product-detail): add ProductDetailDescription view component

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailImagesSliderComponentPartail.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailImagesSliderComponentPartail:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
