083d19487822800024559c5680ff907102b53de6
MELEK DEMİR <melekdmrr01@gmail.com>
2025-12-18 14:00:31 +0300

    Add FeatureProduct default view component

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponent/_FeatureProductDefaultComponents.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponent
+{
+    public class _FeatureProductDefaultComponents : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    
+    }
+}
