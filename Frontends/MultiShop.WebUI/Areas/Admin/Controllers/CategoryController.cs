23a8ab2e42ab6250b516bb1cdd231e1636ab2190
MELEK DEMİR <melekdmrr01@gmail.com>
2026-01-12 14:50:52 +0300

    feat: Add create, update, and delete actions for Category in Admin controller

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -33,5 +33,81 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             }
             return View();
         }
+        [HttpGet]
+        [Route("CreateCategory")]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateCategory")]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto); //metni jsona çevir
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMassage = await client.PostAsync("http://localhost:7060/api/Categories", stringContent);
+            if (responseMassage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Category",new {area="Admin"});
+            }
+            return View();
+        }
+      
+
+        [Route("DeleteCategory/{id}")]
+
+
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:7060/api/Categories/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+
+      
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Güncelleme Sayfası";
+            ViewBag.v0 = "Kategori İşlemleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessge = await client.GetAsync($"http://localhost:7060/api/Categories/{id}");
+            if (responseMessge.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+        [Route("UpdateCategory/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+
+          
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto); //metni json a çevir
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessge = await client.PutAsync($"http://localhost:7060/api/Categories/",stringContent);
+            if (responseMessge.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+
+            }
+            return View();
+        }
     }
 }
