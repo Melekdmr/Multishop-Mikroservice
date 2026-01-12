74337353f79692669f4f138e2aa723d6e1a3cc79
MELEK DEMİR <melekdmrr01@gmail.com>
2026-01-12 10:04:58 +0300

    Add CategoryController with API-based category listing

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Multishop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
 
