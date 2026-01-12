ede2540c93cce5186a2f52c4552d813ce5f9fa3a
MELEK DEMİR <melekdmrr01@gmail.com>
2026-01-12 14:50:20 +0300

    Add UpdateCategoryDto with validation

---
+++ Frontends/Multishop.DtoLayer/CatalogDtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multishop.DtoLayer.CatalogDtos.CategoryDtos
+{
+    public class UpdateCategoryDto
+    {
+        public string CategoryID { get; set; }
+        [Required(ErrorMessage = "Kategori adı zorunludur.")]
+        public string CategoryName { get; set; }
+    }
+}
