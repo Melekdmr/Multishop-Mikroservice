983eb084b1f92ebefaa04267b7bedfb6de5fc240
MELEK DEMİR <melekdmrr01@gmail.com>
2026-01-12 14:50:11 +0300

    Add CreateCategoryDto with validation

---
+++ Frontends/Multishop.DtoLayer/CatalogDtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multishop.DtoLayer.CatalogDtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        [Required(ErrorMessage = "Kategori adı zorunludur.")]
+        public string  CategoryName { get; set; }
+    }
+}
