91b7829f96afbfa554ce7ceeb66fc4b8d552d5b7
MELEK DEMİR <melekdmrr01@gmail.com>
2026-01-13 22:52:38 +0300

    refactor: enrich product DTO with category object

---
+++ Services/Catalog/Multishop.Catalog/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using Multishop.Catalog.Dtos.CategoryDtos;
+using Multishop.Catalog.Entities;
+
+namespace Multishop.Catalog.Dtos.ProductDtos
+{
+    public class ResultProductWithCategoryDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryID { get; set; }
+        //public string CategoryName { get; set; }
+        public ResultCategoryDto Category { get; set; }
+     
+    }
+}
